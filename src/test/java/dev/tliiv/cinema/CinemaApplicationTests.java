91ed3e3674a16620d866f0a107362292e4756cc5
Tarmo <liivamagi.tarmo@gmail.com>
2024-03-19 22:50:20 +0200

    BE start with MongoDB connected

---
+++ src/test/java/dev/tliiv/cinema/CinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tliiv.cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
